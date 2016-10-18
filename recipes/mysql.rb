f8e2167161f47b73d713a424992824e0dbc84ed8
Hank Ehly <henry.ehly@gmail.com>
2016-10-19 08:08:02 +0900

    chef generate recipe mysql

---
+++ recipes/mysql.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: get-native.com-cookbook
+# Recipe:: mysql
+#
+# Copyright (c) 2016 Hank Ehly, All Rights Reserved.
+
+execute 'update' do
+    command 'sudo apt-get update -y'
+    action :nothing
+end
+
+execute 'upgrade' do
+    command 'sudo apt-get -y upgrade'
+    action :nothing
+end
+
+include_recipe 'build-essential::default'
+package 'psmisc'
+
+mysql_service 'get-native' do
+    version node['get-native']['mysql-version']
+    initial_root_password 'root'
+    bind_address '0.0.0.0'
+    charset 'utf8'
+    run_group 'mysql'
+    run_user 'mysql'
+    port 3306
+    notifies :run, 'execute[update]', :before
+    notifies :run, 'execute[upgrade]', :before
+    action [:create, :start]
+end
