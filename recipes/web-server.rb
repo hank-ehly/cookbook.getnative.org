25a77be79792707024a72f85676037b89c92a3d3
Hank Ehly <henry.ehly@gmail.com>
2016-10-24 14:35:12 +0900

    Add boilerplate for letsencrypt code

--- recipes/web-server.rb
+++ recipes/web-server.rb
@@ -91,3 +91,25 @@ end
 
 include_recipe 'nodejs::nodejs_from_binary'
 include_recipe 'nodejs::npm'
+
+if node['get-native']['environment'] == 'production'
+    # apt_package 'python-letsencrypt-apache'
+
+    # TODO: Not desired state
+    # execute 'letsencrypt --apache'
+
+    # TODO: Not desired state
+    # execute 'letsencrypt renew --dry-run --agree-tos'
+
+    # TODO: Not desired state
+    # TODO: Use systemd_unit recipe
+    # cron 'certbot-auto-renew' do
+    #     minute '0'
+    #     hour '0,12'
+    #     day '*'
+    #     month '*'
+    #     weekday '*'
+    #     user node['get-native']['user']['name']
+    #     command "/usr/bin/env bash /path/to/certbot-auto renew --non-interactive --quiet"
+    # end
+end
