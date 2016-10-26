15e05c4bd23ed6afc0d19c0c0339abfa0d70163a
Hank Ehly <henry.ehly@gmail.com>
2016-10-27 08:24:50 +0900

    Add deploy-key script file with mogi-code

---
+++ files/default/deploy-key.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# 1. GET /repos/:owner/:repo/keys
+# 2. Check if title=... key exists
+# 3. If exists, exit 0
+# 4. Else, POST /repos/:owner/:repo/keys
+        # curl https://api.github.com/...
+        # -H "Accept: application/vnd.github.v3+json"
+        # -u "{github name}:{cat secret-password.txt}"
--- recipes/deploy-key.rb
+++ recipes/deploy-key.rb
@@ -17,11 +17,14 @@ execute 'ssh-keygen' do
     command "ssh-keygen -t rsa -q -f #{node['get-native']['user']['home']}/.ssh/id_rsa -P \"\""
 end
 
-# TODO: Consider script instead
-# curl https://api.github.com/repos/foo/.../keys/... -H "Accept: application/vnd.github.v3+json" -i -u "hank-ehly:..." -X DELETE
-# http_request 'please_delete_me' do
-#     headers node['github']['headers']
-#     url 'https://api.github.com'
-#     message ({:some => 'data'}.to_json)
-#     action :post
-# end
+cookbook_file 'deploy-key.bash' do
+    path '/tmp/deploy-key.bash'
+    source 'deploy-key.bash'
+    owner node['get-native']['user']['name']
+    group node['get-native']['user']['primary_group']
+    mode '0755'
+end
+
+execute 'upload deploy-key' do
+    command '/usr/bin/env bash /tmp/deploy-key.bash'
+end
