4da0871a4c792be284a2432b7b76e1619eaa1e94
Hank Ehly <henry.ehly@gmail.com>
2016-10-27 22:40:50 +0900

    Add SSL Cert installation code as per github example

--- attributes/web-server.rb
+++ attributes/web-server.rb
@@ -27,3 +27,6 @@ default['nodejs']['bin_path'] = '/usr/local/nodejs-binary/bin'
 default['nodejs']['install_method'] = 'binary'
 default['nodejs']['version'] = '6.9.1'
 default['nodejs']['binary']['checksum'] = 'a9d9e6308931fa2a2b0cada070516d45b76d752430c31c9198933c78f8d54b17'
+
+default['acme']['contact'] = [ 'mailto:henry.ehly@gmail.com' ]
+default['acme']['endpoint'] = 'https://acme-v01.api.letsencrypt.org'
--- recipes/letsencrypt.rb
+++ recipes/letsencrypt.rb
@@ -5,15 +5,21 @@
 # Copyright (c) 2016 Hank Ehly, All Rights Reserved.
 
 if node['get-native']['environment'] == 'production' || 'staging'
+    include_recipe 'acme'
+
+    site = node['get-native']['domain']
+    sans = Array[ "www.#{site}" ]
+
     acme_certificate node['get-native']['domain'] do
-        crt "#{node['apache']['dir']}/ssl/#{node['get-native']['domain']}.crt"
-        key "#{node['apache']['dir']}/ssl/#{node['get-native']['domain']}.key"
-        chain "#{node['apache']['dir']}/ssl/#{node['get-native']['domain']}.pem"
+        crt "#{node['apache']['dir']}/ssl/#{site}.crt"
+        key "#{node['apache']['dir']}/ssl/#{site}.key"
+        chain "#{node['apache']['dir']}/ssl/#{site}.pem"
         method 'http'
         owner 'root'
         group 'root'
         wwwroot node['get-native']['docroot']
+        alt_names sans
         notifies :restart, 'service[apache2]'
-        not_if { ::File.exists?("#{node['apache']['dir']}/ssl/#{node['get-native']['domain']}.crt") }
+        not_if { ::File.exists?("#{node['apache']['dir']}/ssl/#{site}.crt") }
     end
 end
