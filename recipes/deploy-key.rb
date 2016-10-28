cccb0e59aa5aac8885cc3ef03ed275d5988961b4
Hank Ehly <henry.ehly@gmail.com>
2016-10-28 11:15:34 +0900

    Create ssh-config file for github

--- recipes/deploy-key.rb
+++ recipes/deploy-key.rb
@@ -14,11 +14,25 @@ deploy_key node['get-native']['environment'] do
     provider Chef::Provider::DeployKeyGithub
     path "#{node['get-native']['user']['home']}/.ssh"
     credentials({
-            user: data_bag_item('github', 'credentials')['username'],
-            password: data_bag_item('github', 'credentials')['password']
-    })
+                        user: data_bag_item('github', 'credentials')['username'],
+                        password: data_bag_item('github', 'credentials')['password']
+                })
     repo node['get-native']['github']['repo']
     owner node['get-native']['user']['name']
     group node['get-native']['user']['primary_group']
     action :add
+end
+
+template 'github-ssh-config' do
+    source 'ssh-config.erb'
+    path "#{node['get-native']['user']['home']}/.ssh/config"
+    mode 0700
+    owner node['get-native']['user']['name']
+    group node['get-native']['user']['primary_group']
+    variables({
+                      host: 'github.com',
+                      user: 'git',
+                      hostname: 'github.com',
+                      identity_file: "#{node['get-native']['user']['home']}/.ssh/#{node['get-native']['environment']}"
+              })
 end
