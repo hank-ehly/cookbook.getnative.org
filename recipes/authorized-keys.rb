e0cc3c60373e94d85029256c9b35f064cd9cba62
Hank Ehly <henry.ehly@gmail.com>
2016-10-28 10:51:32 +0900

    Create authorized_key string

--- recipes/authorized-keys.rb
+++ recipes/authorized-keys.rb
@@ -12,9 +12,14 @@ end
 
 data_bag = "#{node['get-native']['environment']}-#{node['get-native']['role']}"
 
+authorized_keys = ''
+data_bag_item(data_bag, 'ssh')['authorized_keys'].each do |key|
+    authorized_keys += "#{key}\n"
+end
+
 file 'authorized_keys' do
     path "#{node['get-native']['user']['home']}/.ssh/authorized_keys"
-    content data_bag_item(data_bag, 'public_key')['public_key']
+    content authorized_keys
     mode '0644'
     owner node['get-native']['user']['name']
     group node['get-native']['user']['primary_group']
