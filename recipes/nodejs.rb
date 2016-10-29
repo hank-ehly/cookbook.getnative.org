36db1924a2b47d4e746f5c89ded9e15ccd09ee26
Hank Ehly <henry.ehly@gmail.com>
2016-10-29 22:00:53 +0900

    Add nodejs-binary/bin to PATH

--- recipes/nodejs.rb
+++ recipes/nodejs.rb
@@ -10,3 +10,11 @@ include_recipe 'nodejs::npm'
 %w(gulp-cli typings).each do |pkg|
     nodejs_npm pkg
 end
+
+ruby_block 'Add npm module bin to PATH' do
+    block do
+        f = Chef::Util::FileEdit.new("#{node['get-native']['user']['home']}/.bashrc")
+        f.insert_line_if_no_match(/nodejs-binary/, 'export PATH="/usr/local/nodejs-binary/bin:$PATH"')
+        f.write_file
+    end
+end
