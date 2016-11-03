197bff24381cea23fe10d7ace2e84f9a45223304
Hank Ehly <henry.ehly@gmail.com>
2016-11-03 10:39:21 +0900

    Finish add-user spec

--- spec/unit/recipes/add-user_spec.rb
+++ spec/unit/recipes/add-user_spec.rb
@@ -7,14 +7,27 @@
 require 'spec_helper'
 
 describe 'get-native.com-cookbook::add-user' do
-    context 'When all attributes are default, on an unspecified platform' do
-        let(:chef_run) do
-            runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
-            runner.converge(described_recipe)
+    before(:each) do
+        stub_command('which sudo').and_return(false)
+    end
+
+    context 'On the ubuntu 16.04 platform' do
+        cached(:chef_run) do
+            runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
+                node.normal['get-native']['user']['primary_group'] = 'get-native'
+            end.converge(described_recipe)
         end
 
         it 'converges successfully' do
             expect { chef_run }.to_not raise_error
+        end
+
+        it 'creates the get-native' do
+            expect(chef_run).to create_group('get-native')
+        end
+
+        it 'includes the `sudo` recipe' do
+            expect(chef_run).to include_recipe('sudo::default')
         end
     end
 end
