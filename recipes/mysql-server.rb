33a61f0b0388d6ac383c3be3e0366ce51239e704
Hank Ehly <henry.ehly@gmail.com>
2017-01-19 08:13:38 +0900

    Do not create the get-native database by default

--- recipes/mysql-server.rb
+++ recipes/mysql-server.rb
@@ -12,17 +12,5 @@ mysql_service 'get-native' do
     run_group 'mysql'
     run_user 'mysql'
     port 3306
-    notifies :run, 'execute[upgrade]', :before
     action [:create, :start]
 end
-
-bash 'init-db' do
-    code <<-EOH
-        mysql -u root \
-              --password=#{node['get-native']['mysql']['initial_password']} \
-              -S /var/run/mysql-get-native/mysqld.sock \
-              -e 'CREATE DATABASE IF NOT EXISTS get_native DEFAULT CHARACTER SET UTF8;'
-    EOH
-end
-
-# TODO: Change root password
