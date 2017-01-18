1fad1c76e14b245a93c490b7238db074c70f108c
Hank Ehly <henry.ehly@gmail.com>
2017-01-19 08:05:05 +0900

    Update mysql-client recipe after bugfix

--- recipes/mysql-client.rb
+++ recipes/mysql-client.rb
@@ -6,10 +6,5 @@
 
 mysql_client 'get-native' do
     version node['get-native']['mysql']['version']
-
-    # TODO: Update after bugfix
-    # https://github.com/chef-cookbooks/mysql/issues/471
-    package_version '5.7.16-0ubuntu0.16.04.1'
-
     action :create
 end
