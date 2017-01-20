7586aa5dfd27c15dfabb91d94c48e2c3f29c7555
Hank Ehly <henry.ehly@gmail.com>
2017-01-20 21:03:09 +0900

    Add ip-whitelist

--- attributes/web-server.rb
+++ attributes/web-server.rb
@@ -5,13 +5,8 @@ default['apache']['docroot_dir'] = '/var/www'
 
 default['get-native']['docroot'] = "#{node['apache']['docroot_dir']}/get-native.com/#{node['get-native']['environment']}/current/dist/prod"
 default['get-native']['github']['repo'] = 'hank-ehly/get-native.com'
-default['get-native']['user']['sudo_commands'] = [
-        '/usr/sbin/apachectl',
-        '/usr/sbin/apache2',
-        '/usr/local/bin/node',
-        '/usr/local/bin/npm',
-        '/sbin/iptables'
-]
+default['get-native']['user']['sudo_commands'] = %w(/usr/sbin/apachectl /usr/sbin/apache2 /usr/local/bin/node /usr/local/bin/npm /sbin/iptables)
+default['get-native']['ip-whitelist'] = %w(124.144 124.145 61.206.117.242 153.205.74.244 180.43.58.102 136.61.59.52 118.8.132.92 68.45.149.102 118.238.203.216 127.0.0.1)
 
 default['nodejs']['bin_path'] = '/usr/local/nodejs-binary/bin'
 default['nodejs']['install_method'] = 'binary'
