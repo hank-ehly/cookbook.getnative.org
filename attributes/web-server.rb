9a9e8bb9dcc735373fa07ca3ac8522b8f65a743d
Hank Ehly <henry.ehly@gmail.com>
2017-01-27 21:46:35 +0900

    Remove ip whitelist

--- attributes/web-server.rb
+++ attributes/web-server.rb
@@ -9,7 +9,6 @@ default['apache']['timeout'] = 45
 default['get-native']['docroot'] = "#{node['apache']['docroot_dir']}/get-native.com/current/dist/prod"
 default['get-native']['github']['repo'] = 'hank-ehly/get-native.com'
 default['get-native']['user']['sudo_commands'] = %w(/usr/sbin/apachectl /usr/sbin/apache2 /usr/local/bin/node /usr/local/bin/npm /sbin/iptables)
-default['get-native']['ip-whitelist'] = %w(124.144 124.145 61.206.117.242 153.205.74.244 180.43.58.102 136.61.59.52 118.8.132.92 68.45.149.102 118.238.203.216 127.0.0.1)
 
 default['nodejs']['bin_path'] = '/usr/local/nodejs-binary/bin'
 default['nodejs']['install_method'] = 'binary'
