186bb2cbcec1f20c76e606424298daf7bf00c1b9
Hank Ehly <henry.ehly@gmail.com>
2017-03-07 08:11:34 +0900

    Add db-credentials as deploy-user ENV var

--- recipes/mysql-client.rb
+++ recipes/mysql-client.rb
@@ -8,3 +8,14 @@ mysql_client 'get-native' do
     version node['get-native']['mysql']['version']
     action :create
 end
+
+data_bag = "#{node['get-native']['environment']}-#{node['get-native']['role']}"
+db_credentials = data_bag_item(data_bag, 'db-credentials')
+
+ruby_block 'Add db-credentials to deploy user\'s ENV' do
+    block do
+        f = Chef::Util::FileEdit.new("#{node['get-native']['user']['home']}/.bashrc")
+        f.insert_line_if_no_match(/nodejs-binary/, "export GET_NATIVE_DB_PASS=\"#{db_credentials['root_password']}\"")
+        f.write_file
+    end
+end
--- recipes/mysql-server.rb
+++ recipes/mysql-server.rb
@@ -4,13 +4,25 @@
 #
 # Copyright (c) 2016 Hank Ehly, All Rights Reserved.
 
+data_bag = "#{node['get-native']['environment']}-#{node['get-native']['role']}"
+db_credentials = data_bag_item(data_bag, 'db-credentials')
+
 mysql_service 'get-native' do
     version node['get-native']['mysql']['version']
-    initial_root_password node['get-native']['mysql']['initial_password']
+    initial_root_password db_credentials['root_password']
     bind_address node['get-native']['mysql']['bind-address'] || '*'
     charset 'utf8'
     run_group 'mysql'
     run_user 'mysql'
     port 3306
     action [:create, :start]
+end
+
+# This will be used for batch scripting
+ruby_block 'Add db-credentials to deploy user\'s ENV' do
+    block do
+        f = Chef::Util::FileEdit.new("#{node['get-native']['user']['home']}/.bashrc")
+        f.insert_line_if_no_match(/nodejs-binary/, "export GET_NATIVE_DB_PASS=\"#{db_credentials['root_password']}\"")
+        f.write_file
+    end
 end
