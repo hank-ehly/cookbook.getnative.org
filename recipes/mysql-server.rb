8bdad93fb5bf5d6ac754a2e1c013ad9604fa22c8
Hank Ehly <henry.ehly@gmail.com>
2017-03-28 11:54:37 +0900

    Set mysql default time zone to UTC

--- recipes/mysql-server.rb
+++ recipes/mysql-server.rb
@@ -10,7 +10,7 @@ db_credentials = data_bag_item(data_bag, 'db-credentials')
 mysql_service 'get-native' do
     version node['get-native']['mysql']['version']
     initial_root_password db_credentials['root_password']
-    bind_address node['get-native']['mysql']['bind-address'] || '*'
+    bind_address node['get-native']['mysql']['bind-address'] || '*' # todo: defaulting to * is insecure
     charset 'utf8'
     run_group 'mysql'
     run_user 'mysql'
@@ -18,7 +18,14 @@ mysql_service 'get-native' do
     action [:create, :start]
 end
 
-mysql_config 'get-native' do
+mysql_config 'get-native-default-time-zone' do
+    source 'default-time-zone.erb'
+    notifies :restart, 'mysql_service[get-native]'
+    instance 'get-native'
+    action :create
+end
+
+mysql_config 'get-native_max_allowed_packet' do
     source 'max_allowed_packet.erb'
     notifies :restart, 'mysql_service[get-native]'
     instance 'get-native'
