c068b01a412a29fc2bd9789968a72cc6e90fc82a
Hank Ehly <henry.ehly@gmail.com>
2017-04-05 13:36:17 +0900

    Don't allow root login

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,3 +9,4 @@ default['get-native']['mysql']['version'] = '5.7'
 default['get-native']['mysql']['initial_password'] = 'root'
 default['authorization']['sudo']['include_sudoers_d'] = true
 default['openssh']['server']['password_authentication'] = 'no'
+default['openssh']['server']['permit_root_login'] = 'no'
