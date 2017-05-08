3a392dbbd2658d384fa03ed6d4e88b7786df7b8e
Hank Ehly <henry.ehly@gmail.com>
2017-05-09 07:43:54 +0900

    Add script for getting latest node LTS checksum

---
+++ bin/update-node-version.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ ${#} = 0 ]]; then
+    echo "Version not specified. Checking for latest LTS..."
+    NODE_VERSION=$(curl https://nodejs.org/en/ -s | grep -E "^\s+v[0-9]\.[0-9]+\.[0-9] LTS" | awk '{print $1}')
+else
+    if [[ `echo ${1} | grep -E '^v'` ]]; then
+        NODE_VERSION=${1}
+    else
+        NODE_VERSION="v${1}"
+    fi
+fi
+
+CHECKSUM=$(curl https://nodejs.org/dist/${NODE_VERSION}/SHASUMS256.txt.asc -s | grep "node-${NODE_VERSION}-linux-x64.tar.gz" | awk '{print $1}')
+
+echo ${NODE_VERSION}
+echo ${CHECKSUM}
