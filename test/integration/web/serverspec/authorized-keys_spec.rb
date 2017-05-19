34ea0e86ce65f7f2c8621213dca9b09a9ab69c5d
Hank Ehly <henry.ehly@gmail.com>
2017-05-19 18:39:43 +0900

    Add integration tests for authorized-keys recipe

---
+++ test/integration/web/serverspec/authorized-keys_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'get-native.com-cookbook::authorized-keys' do
+    get_native_user = 'get-native'
+    get_native_group = 'get-native'
+
+    describe file("/home/#{get_native_user}/.ssh") do
+        it { should be_directory }
+        it { should be_mode 700 }
+        it { should be_owned_by get_native_user }
+        it { should be_grouped_into get_native_group }
+    end
+
+    describe file("/home/#{get_native_user}/.ssh/authorized_keys") do
+        it { should be_file }
+        it { should be_mode 644 }
+        it { should be_owned_by get_native_user }
+        it { should be_grouped_into get_native_group }
+        its(:size) { should > 0 }
+    end
+end
