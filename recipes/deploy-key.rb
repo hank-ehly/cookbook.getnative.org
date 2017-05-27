f9945b345dbe428228ebdebe7c54d31cb314a68b
Hank Ehly <henry.ehly@gmail.com>
2017-05-27 10:41:28 +0900

    Temporarily disable deploy-key recipe

--- recipes/deploy-key.rb
+++ recipes/deploy-key.rb
@@ -12,29 +12,29 @@ directory ssh_dir do
     group node['get-native']['user']['primary_group']
 end
 
-deploy_key node['get-native']['environment'] do
-    provider Chef::Provider::DeployKeyGithub
-    path ssh_dir
-    credentials({
-        user: data_bag_item('github', 'credentials')['username'],
-        password: data_bag_item('github', 'credentials')['password']
-    })
-    repo node['get-native']['github']['repo']
-    owner node['get-native']['user']['name']
-    group node['get-native']['user']['primary_group']
-    action :add
-end
-
-template 'github-ssh-config' do
-    source 'deploy-key/ssh-config.erb'
-    path "#{ssh_dir}/config"
-    mode 0700
-    owner node['get-native']['user']['name']
-    group node['get-native']['user']['primary_group']
-    variables({
-        host: 'github.com',
-        user: 'git',
-        hostname: 'github.com',
-        identity_file: "#{ssh_dir}/#{node['get-native']['environment']}"
-    })
-end
+# deploy_key node['get-native']['environment'] do
+#     provider Chef::Provider::DeployKeyGithub
+#     path ssh_dir
+#     credentials({
+#         user: data_bag_item('github', 'credentials')['username'],
+#         password: data_bag_item('github', 'credentials')['password']
+#     })
+#     repo node['get-native']['github']['repo']
+#     owner node['get-native']['user']['name']
+#     group node['get-native']['user']['primary_group']
+#     action :add
+# end
+#
+# template 'github-ssh-config' do
+#     source 'deploy-key/ssh-config.erb'
+#     path "#{ssh_dir}/config"
+#     mode 0700
+#     owner node['get-native']['user']['name']
+#     group node['get-native']['user']['primary_group']
+#     variables({
+#         host: 'github.com',
+#         user: 'git',
+#         hostname: 'github.com',
+#         identity_file: "#{ssh_dir}/#{node['get-native']['environment']}"
+#     })
+# end
