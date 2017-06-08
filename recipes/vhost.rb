ef5df990abf637bbe9080fdffd8ebcf9a76fcf5a
Hank Ehly <henry.ehly@gmail.com>
2017-06-08 13:36:33 +0900

    Update vhost recipe

--- recipes/vhost.rb
+++ recipes/vhost.rb
@@ -4,14 +4,13 @@
 #
 # Copyright (c) 2017 Hank Ehly, All Rights Reserved.
 
-# todo: Match directory name to server_name (apache config and other places will also need modification)
-directory "#{node['apache']['docroot_dir']}/get-native.com" do
-    user node['get-native']['user']['name']
-    group node['apache']['group']
-    mode 0755
-end
-
 %W(#{node['get-native']['server_name']} api.#{node['get-native']['server_name']} docs.#{node['get-native']['server_name']}).each do |domain|
+    directory "#{node['apache']['docroot_dir']}/#{domain}" do
+        user node['get-native']['user']['name']
+        group node['apache']['group']
+        mode 0755
+    end
+
     conf_name = File::exist?("#{node['apache']['dir']}/ssl/live/#{domain}/fullchain.pem") ? "#{domain}-ssl" : domain
 
     web_app conf_name do
