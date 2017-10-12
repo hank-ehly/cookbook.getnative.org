d13c57fc4b19fba0208f09f7ed2e70918ff4cf90
Hank Ehly <henry.ehly@gmail.com>
2017-10-11 20:10:43 -0500

    Remove unused letsencrypt logic

--- recipes/letsencrypt.rb
+++ recipes/letsencrypt.rb
@@ -4,14 +4,6 @@
 #
 # Copyright (c) 2016 Hank Ehly, All Rights Reserved.
 
-template '999-dummy.conf' do
-    path "#{node['apache']['dir']}/sites-available/999-dummy.conf"
-    source 'letsencrypt/999-dummy.conf.erb'
-    owner 'root'
-    group 'root'
-    mode 0644
-end
-
 cron 'letsencrypt' do
     command "/usr/bin/letsencrypt renew --config-dir #{node['apache']['dir']}/ssl --agree-tos --email #{node['getnative']['contact']}"
     minute '0'
@@ -20,28 +12,28 @@ cron 'letsencrypt' do
     mailto node['getnative']['contact']
 end
 
-domains = %W(#{node['getnative']['server_name']} api.#{node['getnative']['server_name']} admin.#{node['getnative']['server_name']})
-
-domains.each do |d|
-    bash 'letsencrypt' do
-        code <<-EOH
-            /usr/bin/letsencrypt --domains #{domains.join(',')} \
-                                 --apache \
-                                 --agree-tos \
-                                 --uir \
-                                 --hsts \
-                                 --non-interactive \
-                                 --email #{node['getnative']['contact']} \
-                                 --config-dir #{node['apache']['dir']}/ssl \
-                                 --apache-server-root #{node['apache']['dir']} \
-                                 --apache-vhost-root #{node['apache']['dir']}/sites-available \
-                                 --apache-le-vhost-ext="-ssl.conf" \
-                                 --redirect \
-                                 --logs-dir #{node['apache']['log_dir']}
-        EOH
-        not_if {File::exist?("#{node['apache']['dir']}/ssl/live/#{d}/fullchain.pem")}
-    end
-end
+#domains = %W(#{node['getnative']['server_name']} api.#{node['getnative']['server_name']} admin.#{node['getnative']['server_name']})
+#
+#domains.each do |d|
+#    bash 'letsencrypt' do
+#        code <<-EOH
+#            /usr/bin/letsencrypt --domains #{domains.join(',')} \
+#                                 --apache \
+#                                 --agree-tos \
+#                                 --uir \
+#                                 --hsts \
+#                                 --non-interactive \
+#                                 --email #{node['getnative']['contact']} \
+#                                 --config-dir #{node['apache']['dir']}/ssl \
+#                                 --apache-server-root #{node['apache']['dir']} \
+#                                 --apache-vhost-root #{node['apache']['dir']}/sites-available \
+#                                 --apache-le-vhost-ext="-ssl.conf" \
+#                                 --redirect \
+#                                 --logs-dir #{node['apache']['log_dir']}
+#        EOH
+#        not_if {File::exist?("#{node['apache']['dir']}/ssl/live/#{d}/fullchain.pem")}
+#    end
+#end
 
 #
 # Actual command used:
