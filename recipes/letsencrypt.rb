eedbd788e2e08829b767ef517afe5069ae6128c1
Hank Ehly <henry.ehly@gmail.com>
2017-10-19 07:24:45 +0900

    Update vhost files for subdomain redirects

--- recipes/letsencrypt.rb
+++ recipes/letsencrypt.rb
@@ -12,34 +12,11 @@ cron 'letsencrypt' do
     mailto node['getnative']['contact']
 end
 
-#domains = %W(#{node['getnative']['server_name']} api.#{node['getnative']['server_name']} admin.#{node['getnative']['server_name']})
 #
-#domains.each do |d|
-#    bash 'letsencrypt' do
-#        code <<-EOH
-#            /usr/bin/letsencrypt --domains #{domains.join(',')} \
-#                                 --apache \
-#                                 --agree-tos \
-#                                 --uir \
-#                                 --hsts \
-#                                 --non-interactive \
-#                                 --email #{node['getnative']['contact']} \
-#                                 --config-dir #{node['apache']['dir']}/ssl \
-#                                 --apache-server-root #{node['apache']['dir']} \
-#                                 --apache-vhost-root #{node['apache']['dir']}/sites-available \
-#                                 --apache-le-vhost-ext="-ssl.conf" \
-#                                 --redirect \
-#                                 --logs-dir #{node['apache']['log_dir']}
-#        EOH
-#        not_if {File::exist?("#{node['apache']['dir']}/ssl/live/#{d}/fullchain.pem")}
-#    end
-#end
-
-#
-# Actual command used:
+# Obtain TLS Certificate Manually:
 #
 # sudo /usr/bin/letsencrypt
-#   --domains docs.stg.getnativelearning.com
+#   --domains stg.getnativelearning.com,www.stg.getnativelearning.com
 #   --apache
 #   --non-interactive
 #   --agree-tos
